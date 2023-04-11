7c5e9cfdbf492083f5c069b4be4e274fc11c61a7
mauneven <mauneven@gmail.com>
2023-04-10 21:59:57 -0500

    categories fix

--- front/src/components/categories/Categories.js
+++ front/src/components/categories/Categories.js
@@ -2,15 +2,15 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 
 const categories = [
-  { id: 1, nombre: 'Tecnología', subcategorias: ['Portátiles', 'PC de escritorio', 'Celulares', 'Tablets', 'Smartwatches', 'Accesorios para Dispositivos', 'Cámaras y Fotografía', 'Audio y Sonido', 'Consolas de Videojuegos'] },
-  { id: 2, nombre: 'Ropa', subcategorias: ['Camisas', 'Camisetas', 'Chaquetas', 'Abrigos', 'Pantalones', 'Faldas', 'Vestidos', 'Zapatos', 'Tenis', 'Botas', 'Sandalias', 'Zapatillas', 'Sombreros'] },
-  { id: 3, nombre: 'Electrodomésticos', subcategorias: ['Refrigeradores', 'Lavadoras', 'Secadoras', 'Cafeteras', 'Microondas', 'Licuadoras', 'Aspiradoras', 'Planchas'] },
-  { id: 4, nombre: 'Salud y Belleza', subcategorias: ['Maquillaje', 'Cuidado de la Piel', 'Cuidado del Cabello', 'Fragancias', 'Suplementos y Vitaminas', 'Masajeadores y Relajación'] },
-  { id: 5, nombre: 'Vehículos', subcategorias: ['Automóviles', 'Motocicletas', 'Camionetas', 'Bicicletas', 'Botes'] },
-  { id: 6, nombre: 'Del Hogar', subcategorias: ['Muebles', 'Decoración', 'Iluminación', 'Baño', 'Cocina y Comedor', 'Exterior y Jardín', 'Suministros de Limpieza'] },
-  { id: 7, nombre: 'Vivienda y Hospedaje', subcategorias: ['Casas en Arriendo', 'Casas en Venta', 'Apartamentos en Arriendo', 'Apartamentos en Venta', 'Apartaestudios', 'Casa Quinta en Arriendo', 'Habitaciones', 'Terrenos'] },
-  { id: 8, nombre: 'Suscripciones', subcategorias: ['Netflix', 'Spotify', 'Apple Music', 'HBO Max', 'Crunchyroll', 'Tidal', 'Amazon Prime Video', 'Disney+', 'Xbox Game Pass', 'Play Station Plus', 'Discord Nitro'] },
-  { id: 9, nombre: 'Maquinaria Pesada', subcategorias: ['Volquetas', 'Excavadoras', 'Motoniveladoras', 'Retroexcavadoras', 'Montacargas', 'Grúas', 'Bulldozers', 'Cargadores Frontales'] },
+  { id: 1, name: 'Tecnología', subcategories: ['Portátiles', 'PC de escritorio', 'Celulares', 'Tablets', 'Smartwatches', 'Accesorios para Dispositivos', 'Cámaras y Fotografía', 'Audio y Sonido', 'Consolas de Videojuegos'] },
+  { id: 2, name: 'Ropa', subcategories: ['Camisas', 'Camisetas', 'Chaquetas', 'Abrigos', 'Pantalones', 'Faldas', 'Vestidos', 'Zapatos', 'Tenis', 'Botas', 'Sandalias', 'Zapatillas', 'Sombreros'] },
+  { id: 3, name: 'Electrodomésticos', subcategories: ['Refrigeradores', 'Lavadoras', 'Secadoras', 'Cafeteras', 'Microondas', 'Licuadoras', 'Aspiradoras', 'Planchas'] },
+  { id: 4, name: 'Salud y Belleza', subcategories: ['Maquillaje', 'Cuidado de la Piel', 'Cuidado del Cabello', 'Fragancias y Perfumes', 'Suplementos y Vitaminas', 'Masajeadores y Relajación'] },
+  { id: 5, name: 'Vehículos', subcategories: ['Automóviles', 'Motocicletas', 'Camionetas', 'Bicicletas', 'Botes'] },
+  { id: 6, name: 'Del Hogar', subcategories: ['Muebles', 'Decoración', 'Iluminación', 'Baño', 'Cocina y Comedor', 'Exterior y Jardín', 'Suministros de Limpieza'] },
+  { id: 7, name: 'Vivienda y Hospedaje', subcategories: ['Casas en Arriendo', 'Casas en Venta', 'Apartamentos en Arriendo', 'Apartamentos en Venta', 'Apartaestudios', 'Casa Quinta en Arriendo', 'Habitaciones', 'Terrenos'] },
+  { id: 8, name: 'Suscripciones', subcategories: ['Netflix', 'Spotify', 'Apple Music', 'HBO Max', 'Crunchyroll', 'Tidal', 'Amazon Prime Video', 'Disney+', 'Xbox Game Pass', 'Play Station Plus', 'Discord Nitro'] },
+  { id: 9, name: 'Maquinaria Pesada', subcategories: ['Volquetas', 'Excavadoras', 'Motoniveladoras', 'Retroexcavadoras', 'Montacargas', 'Grúas', 'Bulldozers', 'Cargadores Frontales'] },
 ];
 
 export default function PostCategory({ onMainCategoryChange, onSubcategoryChange, externalSelectedSubcategory, isRequired = false  }) {
