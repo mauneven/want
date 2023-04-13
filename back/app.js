a81cbc1cd06751d0d92da6bcc719032f9b208b81
mauneven <mauneven@gmail.com>
2023-04-12 19:16:24 -0500

    show user info on postslist

--- back/app.js
+++ back/app.js
@@ -69,19 +69,11 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke!');
 });
 
-// para la main
-
-/* const options = {
+const options = {
   key: fs.readFileSync('/etc/letsencrypt/live/want.com.co/privkey.pem'),
   cert: fs.readFileSync('/etc/letsencrypt/live/want.com.co/fullchain.pem')
 };
 
 https.createServer(options, app).listen(4000, () => {
   console.log('Server started on port 4000');
-}); */
-
-// para la develop
-
-app.listen(4000, () =>{
-  console.log("servidor iniciado en el 4000")
-})
+}); 
