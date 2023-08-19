baaa0dd512598163264620210544de77f85e5636
mauneven <mauneven@gmail.com>
2023-08-19 14:24:15 -0500

    how to get on main or dev

--- frontv2/lib/config/connections/api_config.dart
+++ frontv2/lib/config/connections/api_config.dart
@@ -2,8 +2,10 @@ import 'package:universal_io/io.dart';
 
 String getServerUrl() {
   if (Platform.isAndroid || Platform.isIOS) {
+    // https://want.com.co for main
     return 'http://10.0.2.2:4000';
   } else {
+    // same for web
     return 'http://localhost:4000';
   }
 }
