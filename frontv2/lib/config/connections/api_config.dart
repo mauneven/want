e01485cc8d60d43867d5622349929ecf026a3bf0
mauneven <mauneven@gmail.com>
2023-08-20 12:00:20 -0500

    IOS environment (iphone)

--- frontv2/lib/config/connections/api_config.dart
+++ frontv2/lib/config/connections/api_config.dart
@@ -1,9 +1,13 @@
 import 'package:universal_io/io.dart';
 
 String getServerUrl() {
-  if (Platform.isAndroid || Platform.isIOS) {
-    // https://want.com.co for main
-    return 'http://10.0.2.2:4000';
+  if (Platform.isAndroid || Platform.isIOS || Platform.isMacOS) {
+    if (Platform.isAndroid){
+      return 'http://10.0.2.2:4000';
+    }
+    else {
+      return 'http://127.0.0.1:4000';
+    }
   } else {
     // same for web
     return 'http://localhost:4000';
