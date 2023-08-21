67342663aa2f9fe14949d2def4d339e3b05e10c3
mauneven <mauneven@gmail.com>
2023-08-21 13:12:43 -0500

    login on web gets user

--- frontv2/lib/components/auth/auth_service.dart
+++ frontv2/lib/components/auth/auth_service.dart
@@ -1,36 +1,59 @@
 import 'dart:convert';
 import 'package:dio/dio.dart';
 import 'package:dio_cookie_manager/dio_cookie_manager.dart';
+import 'package:universal_io/io.dart';
 import 'package:cookie_jar/cookie_jar.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import '../../config/connections/api_config.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import '../../config/connections/api_config.dart';
 
 class AuthService {
   final Dio dio;
-  final CookieJar cookieJar;
+  final CookieJar? cookieJar;
   final storage = FlutterSecureStorage();
 
   AuthService()
       : dio = Dio(),
-        cookieJar = CookieJar() {
-    dio.interceptors.add(CookieManager(cookieJar));
+        cookieJar = (Platform.isAndroid || Platform.isIOS) ? CookieJar() : null {
+    if (Platform.isAndroid || Platform.isIOS) {
+      dio.interceptors.add(CookieManager(cookieJar!));
+    }
+
+// Configura Dio para enviar credenciales en la web
+    if (!Platform.isAndroid && !Platform.isIOS) {
+      dio.options.responseType = ResponseType.json;
+      dio.options.followRedirects = false;
+      dio.options.validateStatus = (status) => status! < 500;
+      dio.options.headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': 'true',
+      };
+      dio.options.extra = {
+        'withCredentials': true,
+      };
+    }
   }
 
   Future<void> saveCookies() async {
-    final cookies = await cookieJar.loadForRequest(Uri.parse(getServerUrl()));
-    final cookiesList = cookies.map((cookie) => cookie.toString()).toList();
-    print('Cookies saved: $cookiesList');
-    await storage.write(key: 'cookies', value: jsonEncode(cookiesList));
+    if (Platform.isAndroid || Platform.isIOS) {
+      final cookies = await cookieJar!.loadForRequest(Uri.parse(getServerUrl()));
+      final cookiesList = cookies.map((cookie) => cookie.toString()).toList();
+      print('Cookies saved: $cookiesList');
+      await storage.write(key: 'cookies', value: jsonEncode(cookiesList));
+    } else {
+// En la web, las cookies son manejadas automáticamente por el navegador
+      print('Cookies for web are handled by the browser.');
+    }
   }
 
   Future<void> loadCookies() async {
-    final cookiesString = await storage.read(key: 'cookies');
-    if (cookiesString != null) {
-      final cookiesStringList = jsonDecode(cookiesString) as List;
-      final cookies = cookiesStringList.map((cookieString) => Cookie.fromSetCookieValue(cookieString as String)).toList();
-      final uri = Uri.parse(getServerUrl());
-      cookieJar.saveFromResponse(uri, cookies);
+    if (Platform.isAndroid || Platform.isIOS) {
+      final cookiesString = await storage.read(key: 'cookies');
+      if (cookiesString != null) {
+        final cookiesStringList = jsonDecode(cookiesString) as List;
+        final cookies = cookiesStringList.map((cookieString) => Cookie.fromSetCookieValue(cookieString as String)).toList();
+        final uri = Uri.parse(getServerUrl());
+        cookieJar!.saveFromResponse(uri, cookies);
+      }
     }
   }
 
@@ -52,5 +75,4 @@ class AuthService {
     }
     return false; // Retornar false si el usuario no está presente
   }
-
 }
