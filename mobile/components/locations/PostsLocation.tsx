f57aa39c99a87a6d2e7167855df1f259992c69e9
mauneven <mauneven@gmail.com>
2023-08-26 14:33:30 -0500

    postsmaps (no search integrated)

---
+++ mobile/components/locations/PostsLocation.tsx
@@ -0,0 +1,127 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { View, Modal, Button, TextInput, StyleSheet } from 'react-native';
+import MapView, { Marker, Circle, Region } from 'react-native-maps';
+import axios from 'axios';
+import { Picker } from '@react-native-picker/picker';
+
+interface Location {
+  latitude: number;
+  longitude: number;
+}
+
+interface PostsLocationProps {
+  onFilterChange: (filter: { latitude: number; longitude: number; radius: number }) => void;
+}
+
+const PostsLocation: React.FC<PostsLocationProps> = ({ onFilterChange }) => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [currentLocation, setCurrentLocation] = useState<Location | null>(null);
+  const [radius, setRadius] = useState<number>(1000); // Default radius in meters
+  const [searchQuery, setSearchQuery] = useState<string>('');
+  const [searchResults, setSearchResults] = useState<any[]>([]);
+  const mapRef = useRef<MapView | null>(null);
+
+  useEffect(() => {
+    // Get initial location
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          const { latitude, longitude } = position.coords;
+          setCurrentLocation({ latitude, longitude });
+        },
+        error => console.log(error),
+        { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+      );
+    }
+  }, []);
+
+  const fetchSearchResults = async (query: string) => {
+    try {
+      const response = await axios.get(`https://nominatim.openstreetmap.org/search?format=json&q=${query}`);
+      setSearchResults(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleSearchChange = (query: string) => {
+    setSearchQuery(query);
+    fetchSearchResults(query);
+  };
+
+  const handleSearchSelect = (location: any) => {
+    const { lat, lon } = location;
+    const newLocation = { latitude: parseFloat(lat), longitude: parseFloat(lon) };
+    setCurrentLocation(newLocation);
+    if (mapRef.current) {
+      const newRegion: Region = {
+        ...newLocation,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      };
+      mapRef.current.animateToRegion(newRegion, 500);
+    }
+    setSearchQuery('');
+    setSearchResults([]);
+  };
+
+  const handleRadiusChange = (value: number) => {
+    setRadius(value);
+  };
+
+  useEffect(() => {
+    if (currentLocation && radius) {
+      onFilterChange({ latitude: currentLocation.latitude, longitude: currentLocation.longitude, radius });
+    }
+  }, [currentLocation, radius]);
+
+  return (
+    <View>
+      <Button title="Mostrar Mapa" onPress={() => setModalVisible(true)} />
+      <Modal
+        animationType="slide"
+        transparent={false}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={{ flex: 1 }}>
+          <TextInput
+            style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+            onChangeText={text => handleSearchChange(text)}
+            value={searchQuery}
+            placeholder="Buscar ubicación"
+          />
+          {searchResults.map((result, index) => (
+            <Button key={index} title={result.display_name} onPress={() => handleSearchSelect(result)} />
+          ))}
+          <Picker
+            selectedValue={radius}
+            onValueChange={(itemValue) => handleRadiusChange(itemValue)}
+          >
+            <Picker.Item label="1 km" value={1} />
+            <Picker.Item label="5 km" value={5} />
+            <Picker.Item label="10 km" value={10} />
+            {/* Add more as needed */}
+          </Picker>
+          {currentLocation && (
+            <MapView
+              ref={mapRef}
+              style={{ flex: 1 }}
+              initialRegion={{
+                ...currentLocation,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+              }}
+            >
+              <Marker coordinate={currentLocation} />
+              <Circle center={currentLocation} radius={radius} />
+            </MapView>
+          )}
+          <Button title="Cerrar Mapa" onPress={() => setModalVisible(false)} />
+        </View>
+      </Modal>
+    </View>
+  );
+};
+
+export default PostsLocation;
--- mobile/screens/HomeScreen.tsx
+++ mobile/screens/HomeScreen.tsx
@@ -3,6 +3,7 @@ import { View, FlatList, ListRenderItemInfo } from 'react-native';
 import axios from 'axios';
 import { useTheme } from '@react-navigation/native';
 import PostCard from '../components/posts/PostCard';
+import PostsLocation from '../components/locations/PostsLocation';
 
 interface Post {
   _id: string;
@@ -12,49 +13,72 @@ interface Post {
   photos: string[];
 }
 
+interface LocationFilter {
+  latitude: number;
+  longitude: number;
+  radius: number;
+}
+
 const HomeScreen = () => {
   const [posts, setPosts] = useState<Post[]>([]);
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
   const [hasMore, setHasMore] = useState(true);
-  const { colors } = useTheme(); // Utiliza los colores del tema actual
+  const [locationFilter, setLocationFilter] = useState<LocationFilter | null>(null);
+  const { colors } = useTheme();
 
   const fetchPosts = useCallback(async () => {
-    if (!hasMore) return;
+    if (!hasMore || loading) return;
 
+    setLoading(true);
     try {
-      setLoading(true);
-      const response = await axios.get(`https://want.com.co/api/posts?page=${page}`);
-      
+      const url = `https://want.com.co/api/posts?page=${page}`;
+      const params = locationFilter ? {
+        latitude: locationFilter.latitude,
+        longitude: locationFilter.longitude,
+        radius: locationFilter.radius,
+      } : {};
+
+      const response = await axios.get(url, { params });
+
       if (response.data.posts.length > 0) {
-        setPosts([...posts, ...response.data.posts]);
+        setPosts(prevPosts => [...prevPosts, ...response.data.posts]);
       } else {
         setHasMore(false);
       }
-      
-      setLoading(false);
     } catch (error) {
-      setLoading(false);
       console.error(error);
+    } finally {
+      setLoading(false);
     }
-  }, [page, posts, hasMore]);
+  }, [page, locationFilter, hasMore, loading]);
 
   useEffect(() => {
+    if (page === 1) {
+      setPosts([]); // Clear existing posts if page is reset
+    }
     fetchPosts();
-  }, [page]);
+  }, [page, locationFilter]);
+
+  const handleLocationFilter = (newFilter: LocationFilter) => {
+    setLocationFilter(newFilter);
+    setPage(1);  // Reset pagination
+    setHasMore(true); // Reset hasMore flag
+  };
 
   const renderPost = ({ item }: ListRenderItemInfo<Post>) => {
-    return <PostCard post={item} />;  // Utiliza el nuevo componente
+    return <PostCard post={item} />;
   };
 
   const handleLoadMore = () => {
     if (!loading && hasMore) {
-      setPage(page + 1);
+      setPage(prevPage => prevPage + 1);
     }
   };
 
   return (
     <View style={{ backgroundColor: colors.background }}>
+      <PostsLocation onFilterChange={handleLocationFilter} />
       <FlatList
         data={posts}
         renderItem={renderPost}
