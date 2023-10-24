30ee52ac2135c40df3d3a9a2a1aab747b3f6009b
mauneven <mauneven@gmail.com>
2023-10-23 20:09:04 -0500

    clean layout

--- frontv2/app/layout.tsx
+++ frontv2/app/layout.tsx
@@ -1,10 +1,3 @@
-import '@mantine/core/styles.css';
-import '@mantine/carousel';
-import '@mantine/code-highlight'
-import '@mantine/dates'
-import '@mantine/hooks'
-import '@mantine/tiptap'
-
 import { MantineProvider, ColorSchemeScript, Container } from '@mantine/core';
 import { Navbar } from '@/components/navigation/Navbar';
 import { Footer } from '@/components/footer/Footer';
@@ -13,22 +6,24 @@ export const metadata = {
   title: 'Want',
   description: 'Want',
 };
+import '@mantine/core/styles.css';
+import '@mantine/carousel';
+import '@mantine/code-highlight';
+import '@mantine/dates';
+import '@mantine/hooks';
+import '@mantine/tiptap';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <head>
-        <ColorSchemeScript />
-      </head>
-      <body>
-        <MantineProvider defaultColorScheme='dark'>
-            <Navbar/>
-            <Container fluid pt={70} pb={40} classNames={{root: classes.globalContainer}}>
-            {children}
-            </Container>
-            <Footer/>
-        </MantineProvider>
-      </body>
-    </html>
+    <>
+      <ColorSchemeScript />
+      <MantineProvider defaultColorScheme='dark'>
+        <Navbar />
+        <Container fluid pt={70} pb={40} classNames={{ root: classes.globalContainer }}>
+          {children}
+        </Container>
+        <Footer />
+      </MantineProvider>
+    </>
   );
 }
